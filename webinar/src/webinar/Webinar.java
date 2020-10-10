cf196da7ecfec9ad9cb9b2357662122dcda66fa5
Jose Manuel <71902073+manuel6845@users.noreply.github.com>
2020-10-09 19:26:56 -0500

    Add files via upload

---
+++ webinar/src/webinar/Webinar.java
@@ -0,0 +1,24 @@
+
+ 
+package webinar;
+
+/**
+ *
+ * @author Jose Manuel
+ */
+public class Webinar {
+
+    
+    public static void main(String[] args) {
+   
+        carro jos=new carro();
+        
+        jos.encender();
+          
+        
+       
+        
+        
+    }
+    
+}
---
+++ webinar/src/webinar/carro.java
@@ -0,0 +1,33 @@
+package webinar;
+
+public class carro {
+
+    public  carro() {
+        //construtor
+          System.out.print("se ha construido objeto");
+    }
+
+    //metodos 
+    public void avanzar() {
+        System.out.print("estoy avanzando");
+                  
+    }
+
+    public void frenar() {
+ System.out.print("frenar");
+ 
+    }
+
+    public void encender() {
+ System.out.print("el carro esta encendido");
+        
+    }
+
+    public void apagar() {
+   System.out.print("estoy apagado");
+}
+    
+      public void gasolina() {
+   System.out.print("no tengo para echarle gasolina");
+}
+      }
